2ddf82be0af315930a7458ebeed9f0fe311fdf10
Tin Le <ttle1s@semo.edu>
2020-11-29 14:59:40 -0600

    project files

---
+++ src/account.java
@@ -0,0 +1,14 @@
+public class account {
+    private String first_name;
+    private String last_name;
+    private int driverID;
+    private int studentID;
+    private int birthday;
+    private char accountType;
+    private String car_make;
+    private char gender;
+    private int backAcc;
+
+
+
+}
